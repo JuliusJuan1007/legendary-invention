21418346154a15cf3d69710f4d879c213a2a1774
JuliusJuan1007 <112535418+JuliusJuan1007@users.noreply.github.com>
2023-09-12 08:53:14 +0700

    Add files via upload

---
+++ Flutter/Dart/tugas_2/bin/tugas_2.dart
@@ -0,0 +1,42 @@
+import 'package:tugas_2/tugas_2.dart' as tugas_2;
+
+void main(List<String> arguments) {
+  int nilai = 25;
+  double nilai2 = 5.3;
+  print(nilai);
+  print(nilai2);
+
+String nama = 'Juan';
+String nama2 = "Belajar Mobile Programming";
+print(nama);
+print(nama2);
+
+bool isLogin = true;
+bool isLogin2 = false;
+print(isLogin);
+print(isLogin2);
+
+List<int> listNilai = [1, 2, 3, 4, 5];
+print(listNilai);
+List<String> listNama = ['Juan', 'Belajar', 'Dicoding'];
+print(listNama);
+
+Map<String, String> mapNama = {
+  'nama': 'Juan',
+  'nama2': 'belajar',
+  'nama3': 'dicoding'
+};
+print(mapNama);
+
+var umur = "Dua puluh tahun";
+print(umur);
+
+int tinggi;
+int? berat;
+String? pesan = null;
+
+const double pi = 3.14;
+final String nama3 = "Juan";
+print(pi);
+print(nama3);
+}
